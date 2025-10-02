5b7d814c7ada582168a37426af1cbff4f772c36a
BibhasShris <143662528+BibhasShris@users.noreply.github.com>
2025-10-01 23:12:58 -0600

    table to-do list

--- to-do-list/src/ToDoTable.jsx
+++ to-do-list/src/ToDoTable.jsx
@@ -2,17 +2,19 @@ import TableHead from "./TableHead.jsx";
 import TaskDetailRow from "./TaskDetailRow.jsx";
 
 export default function ToDoTable() {
-    const task = [
-        {priority: 1, name: "Homework", status: "done", due = "Oct 1"},
-        {priority: 2, name: "Gym", status: "In Progress", due = "Oct 1"},
-        {priority: 3, name: "Laundry", status: "on Hold", due = "Oct 2"},
-    ]; 
+  const tasks = [
+    { priority: 1, name: "Homework", status: "done", due: "Oct 1" },
+    { priority: 2, name: "Gym", status: "In Progress", due: "Oct 1" },
+    { priority: 3, name: "Laundry", status: "on Hold", due: "Oct 2" },
+  ];
   return (
     <table border="1" cellPadding="8">
       <TableHead />
-      <tbody>{task.map(tasks) => (
-        <td>{tasks.priority}, {tasks.name}, {tasks.status}, {task.due}</td>
-      )}</tbody>
+      <tbody>
+        {tasks.map((t, index) => (
+          <TaskDetailRow key={index} task={t} />
+        ))}
+      </tbody>
     </table>
   );
 }

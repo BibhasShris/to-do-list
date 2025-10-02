9d69959bd5c1717eb0584d202b04feba661609be
BibhasShris <143662528+BibhasShris@users.noreply.github.com>
2025-10-02 00:48:44 -0600

    static version final

--- to-do-list/src/ToDoApp.jsx
+++ to-do-list/src/ToDoApp.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import ToDoTable from "./ToDoTable";
+import AddTaskButton from "./AddTaskButton";
 
 export default function ToDoApp() {
   return (
     <div>
       <Header />
       <ToDoTable />
+      <AddTaskButton />
     </div>
   );
 }
